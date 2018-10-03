1a9643868cbb79e1da2e9c8f50ab40b598ea5614
gwendal-orinel <gwendal.orinel@gmail.com>
2018-10-03 14:07:12 +0200

    Update listing.sh

--- my_services_cloud/data/listing.sh
+++ my_services_cloud/data/listing.sh
@@ -5,7 +5,7 @@ echo "\
 names=($name_ports)
 i=0
 for http in $http_ports; do
-        echo ${names[i]:1}": <a target='_blank' href='http://cloudwatt.orinel.net:"$http"'>cloudwatt.orinel.net:"$http"</a></br>"
+        echo ${names[i]:1}": <a target='_blank' href='http://cloudwatt.orinel.net:"$http"'>http://cloudwatt.orinel.net:"$http"</a></br>"
         i=$((i+1))
 done
 
@@ -13,7 +13,7 @@ echo "\
 <h1>Https Services:</h1>"
 i=0
 for https in $https_ports; do
-        echo ${names[i]:1}": <a target='_blank' href='http://cloudwatt.orinel.net:"$https"'>cloudwatt.orinel.net:"$https"</a></br>"
+        echo ${names[i]:1}": <a target='_blank' href='https://cloudwatt.orinel.net:"$https"'>http://cloudwatt.orinel.net:"$https"</a></br>"
         i=$((i+1))
 done
 
