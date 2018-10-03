55d84618380aea97ed5a1e7b0c838ec38d3b00bb
gwendal-orinel <gwendal.orinel@gmail.com>
2018-10-03 15:05:21 +0200

    update

--- my_services_cloud/data/create_vhosts.sh
+++ my_services_cloud/data/create_vhosts.sh
@@ -5,15 +5,26 @@ echo '
 names=($name_ports)
 i=0
 for http in $http_ports; do
-     if [ "${name[i]:1}" == "folio"  ]; then
-     echo '
+
+if [ "${name[i]:1}" == "folio"  ]; then
+echo '
 <VirtualHost *:80> 
       ServerName "gwendal.orinel.net"
       ProxyPass / http://localhost:'$http'/
       ProxyPassReverse / http://localhost:'$http'/
       ProxyPreserveHost On
 </VirtualHost>'	
-     fi
+fi
+
+if [ "${name[i]:1}" == "owncloud"  ]; then
+echo '
+<VirtualHost *:80> 
+      ServerName "cloud.orinel.net"
+      ProxyPass / http://localhost:'$http'/
+      ProxyPassReverse / http://localhost:'$http'/
+      ProxyPreserveHost On
+</VirtualHost>'	
+fi
      
 i=$((i+1))
 done
