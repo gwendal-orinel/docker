c024c68dfcba28d93fc8179106a2cc2e184c0637
gwendal-orinel <gwendal.orinel@gmail.com>
2018-10-03 14:42:41 +0200

    Update create_vhosts.sh

--- my_services_cloud/data/create_vhosts.sh
+++ my_services_cloud/data/create_vhosts.sh
@@ -2,3 +2,17 @@ echo '
 <VirtualHost *:80>
         DocumentRoot "/www/html"
 </VirtualHost>'
+names=($name_ports)
+i=0
+for http in $http_ports; do
+if [ "${name[i]:1}" == "folio"  ]: then
+echo '
+<VirtualHost *:80>
+      ServerName "gwendal.orinel.net"
+      ProxyPass / http://localhost:'$http'/
+      ProxyPassReverse / http://localhost:'$http'/
+      ProxyPreserveHost On
+</VirtualHost>'	
+fi
+i=$((i+1)) 
+done
