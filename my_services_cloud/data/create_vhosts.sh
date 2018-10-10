2b3df6604ed0c96a5a235682a0f96869f1de4ec5
Gwendal Orinel <gwendal.orinel@gmail.com>
2018-10-10 15:52:26 +0200

    update

--- my_services_cloud/data/create_vhosts.sh
+++ my_services_cloud/data/create_vhosts.sh
@@ -16,7 +16,7 @@ echo '
 </VirtualHost>'	
 fi
 
-if [ "${names[i]:1}" == "nextcloud"  ]; then
+if [ "${names[i]:1}" == "nextcloud2"  ]; then
 echo '
 <VirtualHost *:80> 
       ServerName "cloud.orinel.net"
