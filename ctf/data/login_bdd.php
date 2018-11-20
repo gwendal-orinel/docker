87349645dcf490dc0fea68b9119f5dd17ed92bed
Gwendal Orinel <gwendal.orinel@gmail.com>
2018-11-20 22:40:51 +0100

    update-ctf

--- ctf/data/login_bdd.php
+++ ctf/data/login_bdd.php
@@ -1,4 +1,8 @@
 <?php
+$bdd_host=getenv('BDD_HOST');
+$bdd_username=getenv('BDD_USER');
+$bdd_password=getenv('BDD_PASS');
+
 try{
 	$connexion=new PDO("mysql:host=".$bdd_host.";dbname=CTF",$bdd_username,$bdd_password);
 	$connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
