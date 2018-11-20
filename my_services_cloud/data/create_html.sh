7809057065ec8f103be568d8da30efd34d720e07
Gwendal Orinel <gwendal.orinel@gmail.com>
2018-11-20 23:25:19 +0100

    update

--- my_services_cloud/data/create_html.sh
+++ my_services_cloud/data/create_html.sh
@@ -1,11 +1,12 @@
 echo "\
 <html>\
-<h1>Http services:</h1>"
+<h1>Http services:</h1>\
+<?php $host=$_SERVER['SERVER_NAME']; ?>"
 
 names=($name_ports)
 i=0
 for http in $http_ports; do
-        echo ${names[i]:1}": <a target='_blank' href='http://cloudwatt.orinel.net:"$http"'>http://cloudwatt.orinel.net:"$http"</a></br>"
+        echo ${names[i]:1}": <a target='_blank' href='http://<?php echo $host ?>:"$http"'>http://<?php echo $host ?>:"$http"</a></br>"
         i=$((i+1)) 
 done
 
@@ -13,7 +14,7 @@ echo "\
 <h1>Https services:</h1>"
 i=0
 for https in $https_ports; do
-        echo ${names[i]:1}": <a target='_blank' href='https://cloudwatt.orinel.net:"$https"'>https://cloudwatt.orinel.net:"$https"</a></br>"
+        echo ${names[i]:1}": <a target='_blank' href='https://<?php echo $host ?>:"$https"'>https://<?php echo $host ?>:"$https"</a></br>"
         i=$((i+1))
 done
 
