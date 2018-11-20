beecd190eaf84476bb6c5dd44301e85dd79d1b0e
Gwendal Orinel <gwendal.orinel@gmail.com>
2018-11-20 21:35:52 +0100

    Create login_bdd.php

---
+++ ctf/data/login_bdd.php
@@ -0,0 +1,11 @@
+<?php
+try{
+	$connexion=new PDO("mysql:host=".$bdd_host.";dbname=".$bdd_dbname,$bdd_username,$bdd_password);
+	$connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$connexion -> exec("SET CHARACTER SET utf8");
+}
+catch(Exception $e)
+     {
+	    echo '-- Error: Connection database lost or incorrect settings --';
+     }
+?>
