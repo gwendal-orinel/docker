d100ca652eaf3039e411cc55edff964710f14fdd
Gwendal Orinel <gwendal.orinel@gmail.com>
2018-11-20 23:35:14 +0100

    update

--- my_services_cloud/data/create_html.sh
+++ my_services_cloud/data/create_html.sh
@@ -1,12 +1,12 @@
 echo "\
 <html>\
 <h1>Http services:</h1>\
-<?php $host=$_SERVER['SERVER_NAME']; ?>"
+<?php \$host=\$_SERVER['SERVER_NAME']; ?>"
 
 names=($name_ports)
 i=0
 for http in $http_ports; do
-        echo ${names[i]:1}": <a target='_blank' href='http://<?php echo $host ?>:"$http"'>http://<?php echo $host ?>:"$http"</a></br>"
+        echo ${names[i]:1}": <a target='_blank' href='http://<?php echo \$host ?>:"$http"'>http://<?php echo \$host ?>:"$http"</a></br>"
         i=$((i+1)) 
 done
 
@@ -14,7 +14,7 @@ echo "\
 <h1>Https services:</h1>"
 i=0
 for https in $https_ports; do
-        echo ${names[i]:1}": <a target='_blank' href='https://<?php echo $host ?>:"$https"'>https://<?php echo $host ?>:"$https"</a></br>"
+        echo ${names[i]:1}": <a target='_blank' href='https://<?php echo \$host ?>:"$https"'>https://<?php echo \$host ?>:"$https"</a></br>"
         i=$((i+1))
 done
 
