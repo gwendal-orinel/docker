57db443655da8ee8827550139ccfa137aaef7927
gwendal orinel <gwendal.orinel@gmail.com>
2019-06-05 13:40:44 +0000

    silent curl

--- certbot/update-pages.sh
+++ certbot/update-pages.sh
@@ -3,6 +3,6 @@ PROJECTID="12338658"
 
 for domain in $(curl -s --header "PRIVATE-TOKEN: $TOKEN" https://gitlab.com/api/v4/projects/$PROJECTID/pages/domains | jq -r .[].domain);do
 
-curl --request PUT --header "PRIVATE-TOKEN: $TOKEN" --form "certificate=@certs/fullchain1.pem" --form "key=@certs/privkey1.pem" https://gitlab.com/api/v4/projects/$PROJECTID/pages/domains/$domain
+curl -s --request PUT --header "PRIVATE-TOKEN: $TOKEN" --form "certificate=@certs/fullchain1.pem" --form "key=@certs/privkey1.pem" https://gitlab.com/api/v4/projects/$PROJECTID/pages/domains/$domain
 
 done
