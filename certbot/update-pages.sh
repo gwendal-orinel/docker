b19d7719b944ce6c5046e5dd8db3de6d37e29c1a
gwendal orinel <gwendal.orinel@gmail.com>
2019-06-06 07:10:00 +0000

    Update certs for all projects

--- certbot/update-pages.sh
+++ certbot/update-pages.sh
@@ -1,8 +1,12 @@
 #!/bin/bash -e
-PROJECTID="12338658"
+PROJECTS=$(curl -s --header "PRIVATE-TOKEN: $TOKEN" https://gitlab.com/api/v4/users/gwendal-orinel/projects/ | jq .[].id)
 
-for domain in $(curl -s --header "PRIVATE-TOKEN: $TOKEN" https://gitlab.com/api/v4/projects/$PROJECTID/pages/domains | jq -r .[].domain);do
+for PROJECT in $PROJECTS; do
 
-curl -s --request PUT --header "PRIVATE-TOKEN: $TOKEN" --form "certificate=@cert/archive/orinel.net/fullchain1.pem" --form "key=@cert/archive/orinel.net/privkey1.pem" https://gitlab.com/api/v4/projects/$PROJECTID/pages/domains/$domain
+    for DOMAIN in $(curl -s --header "PRIVATE-TOKEN: $TOKEN" https://gitlab.com/api/v4/projects/$PROJECT/pages/domains | jq -r .[].domain);do
+    
+        curl -s --request PUT --header "PRIVATE-TOKEN: $TOKEN" --form "certificate=@cert/archive/orinel.net/fullchain1.pem" --form "key=@cert/archive/orinel.net/privkey1.pem" https://gitlab.com/api/v4/projects/$PROJECT/pages/domains/$DOMAIN
+    
+    done
 
 done
