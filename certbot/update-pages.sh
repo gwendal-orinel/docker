b3b67ba8dc30064e5d75e88e005d0da0f6c8c337
gwendal orinel <gwendal.orinel@gmail.com>
2019-12-02 09:27:58 +0000

    Update update-pages.sh

--- certbot/update-pages.sh
+++ certbot/update-pages.sh
@@ -8,7 +8,7 @@ for PROJECT in $PROJECTS; do
 
     for DOMAIN in $(curl -s --header "PRIVATE-TOKEN: $TOKEN" https://gitlab.com/api/v4/projects/$PROJECT/pages/domains | jq -r .[].domain);do
     
-        curl -s --request PUT --header "PRIVATE-TOKEN: $TOKEN" --form "certificate=@cert/archive/orinel.net/$LAST_CERT" --form "key=@cert/archive/orinel.net/$LAST_KEY" https://gitlab.com/api/v4/projects/$PROJECT/pages/domains/$DOMAIN
+        curl -s --request PUT --header "PRIVATE-TOKEN: $TOKEN" --form "certificate=@$LAST_CERT" --form "key=@$LAST_KEY" https://gitlab.com/api/v4/projects/$PROJECT/pages/domains/$DOMAIN
     
     done
 
